2f19a3b66e7e1a0fb89fe70db2e0d0c9c864d9bb
Макс <maxim__90.90@mail.ru>
2023-02-02 16:00:26 +0400

    123

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,49 @@
 public class Main {
     public static void main(String[] args) {
+
         task1();
         task2();
+        task3();
     }
 
-    public static void task1() {
+        public static void task1() {
+            System.out.println("Задача 1");
 
-
-        System.out.println("Задача 1");
-
-        int age = 19;
-        double salary = 45_000;
-        double creditLimit;
-
-        if (age >= 23) {
-            creditLimit = salary * 3;
-        } else {
-            creditLimit = salary * 2;
-        }
-        if (salary >= 50_000 && salary < 80_000) {
-            creditLimit = creditLimit * 1.2;
-
-        } else if (salary >= 80_000) {
-            creditLimit = creditLimit * 1.5;
-
-        }
-        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + creditLimit + " рублей");
-
+        int a = 12;
+        int b = 27;
+        int c = 44;
+        int d = 15;
+        int e = 9;
+        double result;
+        result = -1*(a * (b + (c - d * e)));
+        System.out.println(result);
 
     }
 
     public static void task2() {
         System.out.println("Задача 2");
-        int age = 25;
-        double salary = 90_000;
-        double wantedSum = 220_000;
-        double baseBid = 10;
-        int term = 12;
-        double maxPayment = salary * 0.5;
+        int a = 5;
+        int b = 7;
+        a = b;
+        b = a - 2;
 
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
 
-        if (salary > 80_000) {
-            baseBid = baseBid - 0.7;
-        }
-        if (age < 23) {
-            baseBid = baseBid + 1;
-        } else if (age >= 23 && age < 30) {
-            baseBid = baseBid + 0.5;
-        }
+        a = a + b;
+        b = a - b;
+        a = a - b;
 
-        double payment = wantedSum / term + ((wantedSum * baseBid / 100) / term);
-        if (maxPayment >= payment) {
-            System.out.println("«Максимальный платеж при ЗП " + salary + " равен "
-                    + maxPayment + " рублей. Платеж по кредиту " + payment + " рублей. Одобрено");
-        } else {
-            System.out.println("Кредит отказан");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int a = 567;
+        int b = (a%100-a%10)/10;
+        System.out.println(b);
         }
     }
-}
-
 
 
 
